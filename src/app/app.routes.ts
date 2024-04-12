e8dc9300b3497e7b7009d25508d9fcb204f9f26f
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-04-12 10:06:35 +0100

    Learn routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
-
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from './components/second/second.component';
 export const routes: Routes = [];
